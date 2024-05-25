197fdbd4852742a079cbc272efe0aeedf014c985
awbbaughman <144296345+awbbaughman@users.noreply.github.com>
2024-05-25 16:59:06 -0400

    Add files via upload

--- mongodb-app/server.js
+++ mongodb-app/server.js
@@ -1,23 +1,16 @@
 const express = require("express");
-let dbConnect = require("./dbConnect"); 
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 require("dotenv").config();
 
 app.use('/api', apiRoutes);
 
-// parse requests of content-type - application/json
-        app.use(express.json());
-    app.get("/", (req, res) => {
-res.json({ message: "Welcome to my MongoDB application." });
-});
+app.use(express.json());
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 
-let userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
-    app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port${PORT}.`);
 });
